a6cb6fa835bb45dde7c5a09ee0c627e4eda70ba7
niyonagize6369 <niyonagizerachel10@gmail.com>
2025-03-28 11:46:39 +0200

    small project pushed

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -30,7 +30,7 @@ export default function Login() {
   return (
     <div className="pt-20">
       <h3 className="text-3xl text-center text-gray-200">Login</h3>
-      {error && <p className="text-red-500 text-sm text-center">{error}</p>}
+      {error && <p className="text-sm text-center text-red-500">{error}</p>}
 
       <form onSubmit={handleLogin} className="block mb-10 text-center">
         <label className="block pt-5 text-white">Email</label>
@@ -40,7 +40,7 @@ export default function Login() {
           placeholder="Enter Your Email"
           value={loginData.email}
           onChange={handleChange}
-          className="bg-white border border-gray-900 rounded-full w-96 p-2 focus:outline-none text-black focus:border-gray-900"
+          className="p-2 text-black bg-white border border-gray-900 rounded-full w-96 focus:outline-none focus:border-gray-900"
         />
 
         <label className="block mb-2 text-white">Password</label>
@@ -50,10 +50,10 @@ export default function Login() {
           placeholder="Enter your Password"
           value={loginData.password}
           onChange={handleChange}
-          className="bg-white border border-gray-900 rounded-full w-96 p-2 focus:outline-none text-black focus:border-gray-900"
+          className="p-2 text-black bg-white border border-gray-900 rounded-full w-96 focus:outline-none focus:border-gray-900"
         />
 
-        <div className="flex gap-3 justify-center mt-5">
+        <div className="flex justify-center gap-3 mt-5">
           <button
             type="submit"
             className="flex items-center gap-2 px-4 py-2 text-white rounded-full shadow-lg bg-gradient-to-r from-gray-700 to-orange-500 hover:scale-105"
@@ -63,7 +63,7 @@ export default function Login() {
         </div>
       </form>
 
-      <p className="text-sm text-center text-gray-200 mt-3">
+      <p className="mt-3 text-sm text-center text-gray-200">
         Don't have an account?{" "}
         <a href="/" className="text-blue-500">
           Register
